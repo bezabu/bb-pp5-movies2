256567219ef41017c412d8454d20f35f25914c5c
Ben Butler <benbutleranimator@gmail.com>
2024-07-01 11:38:04 +0000

    tweak styling for App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import styles from "./App.module.css";
 import NavBar from './components/NavBar';
 import Button from 'react-bootstrap/Button';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={styles.App}>
+      <header>
         <NavBar />
-        <h1>Hello World!</h1>
-        <Button variant="primary">Primary</Button>
+        <br>
+        </br><br></br><br></br><br></br>
+        <h2>Hello World!</h2>
+        <p>placeholder text</p>
+        <a href="#">placeholder link</a>
+        <br></br><br></br>
+        <Button variant="light">Primary</Button>
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,19 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-
 import film_logo from '../assets/film_logo.png'
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
-  <Navbar.Brand><img src={film_logo} alt="film logo" height="50"/></Navbar.Brand>
+    <Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
+  <Navbar.Brand><Container><img src={film_logo} alt="film logo" height="50"/><h1>Movie Reviews</h1></Container></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+    <Nav className="ml-auto text-left">
+      <Nav.Link><i class="fa-solid fa-house"></i>Home</Nav.Link>
+      <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
+      <Nav.Link><i class="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
