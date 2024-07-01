83aa45efe291f5cd73f2b9aca457ae2660fed56c
Ben Butler <benbutleranimator@gmail.com>
2024-07-01 11:49:08 +0000

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import styles from "./App.module.css";
 import NavBar from './components/NavBar';
 import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className={styles.App}>
-      <header>
+      
         <NavBar />
-        <br>
-        </br><br></br><br></br><br></br>
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" render={() => <h2>Home</h2>} />
+            <Route exact path="/signin" render={() => <h2>Sign in</h2>} />
+          </Switch>
+        
+        
+        </Container>
         <h2>Hello World!</h2>
         <p>placeholder text</p>
         <a href="#">placeholder link</a>
         <br></br><br></br>
         <Button variant="light">Primary</Button>
-      </header>
+      
     </div>
   );
 }
