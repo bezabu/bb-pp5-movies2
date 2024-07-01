c2bbb7b04d901e06ddaeddb11c0bc272c66662c7
Ben Butler <benbutleranimator@gmail.com>
2024-07-01 12:30:07 +0000

    Begin signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './components/NavBar';
 import Button from 'react-bootstrap/Button';
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from 'react-router-dom';
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 
 function App() {
@@ -14,7 +16,7 @@ function App() {
           <Switch>
             <Route exact path="/" render={() => <h2>Home</h2>} />
             <Route exact path="/signin" render={() => <h2>Sign in</h2>} />
-            <Route exact path="/signup" render={() => <h2>Sign up</h2>} />
+            <Route exact path="/signup" render={() => <SignUpForm />} />
             <Route exact path="/popular" render={() => <h2>Popular users</h2>} />
             <Route exact path="/new" render={() => <h2>Newest reviews</h2>} />
             <Route exact path="/movies" render={() => <h2>Movies</h2>} />
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://bb-pp5-movie-review-app-363d95a342e4.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true;
