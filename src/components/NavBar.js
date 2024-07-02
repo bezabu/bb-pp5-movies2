823ad8d3639c2c7c04a09756b89670dd8b826087
Ben Butler <benbutleranimator@gmail.com>
2024-07-02 12:56:33 +0000

    Add CurrentUserContext

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import film_logo from '../assets/film_logo.png'
 import styles from '../styles/NavBar.module.css'
 import  NavLink  from 'react-router-dom/NavLink'
-import { CurrentUserContext } from '../App'
+
+import { useCurrentUser } from '../contexts/CurrentUserContext'
 
 
 
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext)
+  const currentUser = useCurrentUser();
   const loggedOutIcons = <>
   <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i className="fa-solid fa-right-to-bracket"></i>Sign in</NavLink>
   <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup"><i className="fa-solid fa-user-plus"></i>Sign up</NavLink>
